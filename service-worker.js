8357de544e6588948cf91cbf4c1cb2474f112cb4
mn7777903-cyber <mn7777903@gmail.com>
2026-05-27 14:56:07 +0100

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-const CACHE_NAME='wallflix-v51-clean-video-hub';
-self.addEventListener('install',e=>{self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
-self.addEventListener('fetch',e=>{const r=e.request;if(r.mode==='navigate'){e.respondWith(fetch(r,{cache:'no-store'}).catch(()=>caches.match('/index.html')));return}e.respondWith(fetch(r).catch(()=>caches.match(r)))});
